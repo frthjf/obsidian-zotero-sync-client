c924a0671e8a1b1f12ad20ffa8563d8f993b313a
Frithjof Gressmann <frithjof@noc.io>
2023-06-17 20:07:10 -0500

    Integrate zotero-sync client

--- main.ts
+++ main.ts
@@ -1,81 +1,27 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Sync as ZoteroClient } from '@retorquere/zotero-sync'
+import { Store } from '@retorquere/zotero-sync/json-store'
+import { Zotero } from '@retorquere/zotero-sync/typings/zotero'
+import path from 'path';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ZoteroSyncClientSettings {
+	api_key: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ZoteroSyncClientSettings = {
+	api_key: ''
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: ZoteroSyncClientSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new ClientSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.zoteroSync();
 	}
 
 	onunload() {
@@ -89,25 +35,37 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async zoteroSync() {
+		const zotero = new ZoteroClient()
+	  
+		// will emit the following so you can track progress:
+		// emit(Sync.event.library, <library object>, n, total number of libraries). Called at the start of a library sync. Library name is empty for the main library, if there's a name, it's a group library
+		// emit(Sync.event.error, err)
+		// emit(Sync.event.item, <item object>, n, total number of items within library). Called when an item is added/updated
+		// emit(Sync.event.collection, <collection object>, n, number of collections within library). Called when a collection is added/updated
+	  
+		for (const event of [ ZoteroClient.event.library, ZoteroClient.event.collection, ZoteroClient.event.item, ZoteroClient.event.error ]) {
+		  zotero.on(event, (e => function() { console.log(e, [...arguments]) })(event))
+		}
+		await zotero.login(this.settings.api_key)
+		const folder = "References"
+		if (!this.app.vault.getAbstractFileByPath(folder)) {
+			await this.app.vault.createFolder(folder)
+		}
+
+		const directory = path.join(this.app.vault.adapter.basePath, folder)
+		await zotero.sync(await (new Store).load(directory))
+	}
+
+	async parseItem(item: Zotero.Item.Any) {
+		// translates a Zotero data item into a note
+	}
+
+	// 
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class ClientSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -120,17 +78,17 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Zotero API'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('API key')
+			.setDesc('Zotero API key for read-only access')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your secret API key')
+				.setValue(this.plugin.settings.api_key)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					// console.log('API key changed: ' + value);
+					this.plugin.settings.api_key = value;
 					await this.plugin.saveSettings();
 				}));
 	}
